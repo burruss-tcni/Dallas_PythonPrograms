6fc3da7d4d99d40453f2211fd2c9f597ac08c01f
burruss-tcni <73897510+burruss-tcni@users.noreply.github.com>
2025-02-13 08:57:38 -0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,7 @@
 border = %
 print (border * 30 )
 print ("Hello World! This is Python!")
-print (/n)
+print ("/n")
 fn= input("Enter your first name: ") # enter first name
 print("Hi, ", fn, ".")
 print (border * 30 )
